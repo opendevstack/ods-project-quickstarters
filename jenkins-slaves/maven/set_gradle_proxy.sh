7709a80c65e48aa39241f7e5048bd89908b0abb8
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2018-10-04 17:42:57 +0200

    add * wildcards

--- jenkins-slaves/maven/set_gradle_proxy.sh
+++ jenkins-slaves/maven/set_gradle_proxy.sh
@@ -41,6 +41,7 @@ if [[ $HTTP_PROXY != "" ]]; then
 fi
 
 if [[ $NO_PROXY != "" ]]; then
+	noproxy_host=$(echo $NO_PROXY | sed -e "s/,./,*./g")
 	noproxy_host=$(echo $NO_PROXY | sed -e "s/,/|/g")
 	echo "systemProp.http.nonProxyHosts=$noproxy_host" >> $GRADLE_USER_HOME/gradle.properties
 	echo "systemProp.https.nonProxyHosts=$noproxy_host" >> $GRADLE_USER_HOME/gradle.properties

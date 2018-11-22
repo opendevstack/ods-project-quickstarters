5dc2c513e0c0d4d25c0bff5f08781f965f299118
Maria Apsolon <maria.apsolon@boehringer-ingelheim.com>
2018-11-22 16:36:53 +0100

    use icons in the right way, add missing proptypes

--- boilerplates/fe-react/files/src/home/components/Repo.js
+++ boilerplates/fe-react/files/src/home/components/Repo.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { Star } from '@material-ui/icons';
+import Star from '@material-ui/icons/Star';
+import PropTypes from 'prop-types';
 
 const Repo = (props) => (
-    <ListItem>
-      <ListItemIcon>
-        <Star/>
-      </ListItemIcon>
-      <ListItemText>
-        <a href={props.html_url} target="_blank" rel="noopener noreferrer">
-          {props.name}
-        </a>
-      </ListItemText>
-    </ListItem>
-  );
+  <ListItem>
+    <ListItemIcon>
+      <Star/>
+    </ListItemIcon>
+    <ListItemText>
+      <a href={props.html_url} target="_blank" rel="noopener noreferrer">
+        {props.name}
+      </a>
+    </ListItemText>
+  </ListItem>
+);
+
+Repo.propTypes = {
+  html_url: PropTypes.string,
+  name: PropTypes.string,
+};
 
 export default Repo;

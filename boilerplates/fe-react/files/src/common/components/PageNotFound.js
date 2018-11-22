b6097eaba86043515a20b09606497bd2186040e3
Maria Apsolon <maria.apsolon@boehringer-ingelheim.com>
2018-11-22 16:37:08 +0100

    make use of theme

--- boilerplates/fe-react/files/src/common/components/PageNotFound.js
+++ boilerplates/fe-react/files/src/common/components/PageNotFound.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import withStyles from "@material-ui/core/styles/withStyles";
 
-export const PageNotFound = withStyles(({ spacing }) => ({
+export const PageNotFound = withStyles(({ spacing, fontFamily, palette }) => ({
   container: {
     padding: `0 ${spacing.unit * 2}px`,
-    fontFamily: '"Roboto", sans-serif !important',
-    color: '#4a4a4a',
+    fontFamily,
+    color: palette.common.grey,
   },
 }))(({ classes }) => (
   <h2 className={classes.container}>
--- boilerplates/fe-react/files/src/theme.js
+++ boilerplates/fe-react/files/src/theme.js
@@ -4,6 +4,9 @@ export const theme = createMuiTheme({
   palette: {
     primary: { main: '#024c4d'},
     secondary: { main: '#cddc39'},
+    common: {
+      grey: '#4f4f4f'
+    }
   },
   fontFamily: 'Roboto, sans-serif',
   spacing: { unit: 8 }

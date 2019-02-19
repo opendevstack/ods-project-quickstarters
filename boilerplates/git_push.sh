efb7001bfcba7b4a4f2104817c3642f166e79b38
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2019-02-19 11:22:47 +0100

    real clean fix

--- boilerplates/git_push.sh
+++ boilerplates/git_push.sh
@@ -30,8 +30,7 @@ if [ $files == 0 ]; then
 else
 	echo "Upgrading existing project: ${git_url_ssh}"
 	mv "$cloned_git_fld_name"/* .
-	# just doing mv /.* . -> gets a device or resource busy
-	mv "$cloned_git_fld_name"/.g* .
+	mv "$cloned_git_fld_name"/.[!.]* .
 fi
 
 rm -rf "$cloned_git_fld_name"

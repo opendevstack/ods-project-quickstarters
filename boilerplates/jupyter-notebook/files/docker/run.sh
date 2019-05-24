ecb926fb06c8c9bcfec3a7324e375f512944a99d
klingest <stefan.klingelschmitt@boehringer-ingelheim.com>
2019-05-24 10:35:31 +0200

    DSI-16 moved back to CamelCase in JenkinsFile & also included jupyter move to rhel

--- boilerplates/jupyter-notebook/files/docker/run.sh
+++ boilerplates/jupyter-notebook/files/docker/run.sh
@@ -5,23 +5,16 @@ if ! whoami &> /dev/null; then
   fi
 fi
 
-# create jupyter directories
-mkdir -p /opt/app-root/src/share/jupyter
-mkdir -p /opt/app-root/src/share/jupyter/runtime
-mkdir -p /opt/app-root/src/work/storage
+# create directory
+mkdir -p /app/src/work/storage
 
 # set the home directories to a folder with read/write access
-export XDG_DATA_HOME=/opt/app-root
-export HOME=/opt/app-root
+export XDG_DATA_HOME=/app
+export HOME=/app
 
 # link jupyter configs
 export JUPYTER_CONFIG_DIR=/opt/app-root/src/.jupyter
 export JUPYTER_PATH=/opt/app-root/src/share/jupyter
 export JUPYTER_RUNTIME_DIR=/opt/app-root/src/share/jupyter/runtime
-
-source /opt/app-root/bin/activate
-
-# install pip requirements
-pip install -i https://$NEXUS_USERNAME:$NEXUS_PASSWORD@${NEXUS_URL:8}/repository/pypi-all/simple -r requirements.txt
 
 exec jupyter lab

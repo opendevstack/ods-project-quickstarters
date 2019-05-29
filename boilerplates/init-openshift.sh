fc23d6718932267ee1606d8931c919773c6440c9
Krämer,Henrich (IT BI X) BIX-DE-I <henrich.kraemer@boehringer-ingelheim.com>
2019-05-29 12:54:04 +0200

    Generate default component environment in openshift folder of generated project.
    Requires https://github.com/opendevstack/ods-jenkins-shared-library/pull/85
    
    replace upload-templates.sh with tailor #38
    
    Change summary:
    
    - Use yml format allowing exec with multiple lines in code.
    - New templates are in ocp_templates/ocp-config/ and
      configuration in
      ods-configuration/ods-project-quickstarters/templates
    - Provide rundeck jobs for testing ending in <original-name>-qs-38.yaml
    - Improved consistency of rshiny label usage.
      This allows better specificity of tailor commands.
    - Remove namespace entries in templates which appear to not
      be needed.
    
    Scripts in ocp-templates/scripts:
    - create-projects.sh exits when invoked with unknown parameter
    - Some scripts support a -d|--debug flag which causes
      -v to be used in tailor invocations.
    - Some scripts support a --status command so that tailor status is used.
    - Extract portion of create-projects.sh into create-cd-jenkins.sh for
      easier testability.
    - Better output log message.

---
+++ boilerplates/init-openshift.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+set -eu
+
+# Generates default openshift setup.
+#   This is generated in the 'openshift' folder of the specified target directory.
+#
+#   To customize, for example to introduce additional services use a similar
+#   script specific to your quickstarter.
+
+# Get directory of this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+QS_DIR="${SCRIPT_DIR%/*}"
+if [ "${QS_DIR##*/}" != "ods-project-quickstarters" ]; then
+    echo "Bug: should have evaluated to ods-project-quickstarters but was [${QS_DIR##*/}]"
+    exit 2
+fi
+
+# Parameters are passed as environment variables only!
+
+usage_and_exit() {
+    echo "Usage: PROJECT=.. COMPONENT=.. TARGET_DIR=.. ./init-openshift.sh "
+    exit 1;
+}
+
+if [ "$#" -gt 0 ]; then
+    echo "Script does not use parameters."
+    usage_and_exit
+fi
+
+cat <<ECHO_USED_ENV_VARIABLES || usage_and_exit
+Using environment variables:
+  - PROJECT=[$PROJECT]
+  - COMPONENT=[$COMPONENT]
+  - TARGET_DIR=[$TARGET_DIR]
+ECHO_USED_ENV_VARIABLES
+
+OPENSHIFT_DIR="${TARGET_DIR}/openshift"
+if [ -d "${OPENSHIFT_DIR}" ]; then
+    echo "${OPENSHIFT_DIR} already exists, leaving it alone"
+    exit 0;
+fi
+
+mkdir "${OPENSHIFT_DIR}" && cd "${OPENSHIFT_DIR}"
+echo "Generating file in ${PWD}"
+
+# sudo chgrp -R 0 . # asks for password
+
+# iterate over different environments
+# this is based on https://github.com/opendevstack/ods-jenkins-shared-library/pull/85
+environments=(test dev)
+for ENV in "${environments[@]}" ; do
+    # env file
+    echo "Writing env file for tailor:  ${PROJECT}-${ENV}.env"
+    cat << TAILOR_ENV > "${PROJECT}-${ENV}.env"
+PROJECT=${PROJECT}
+COMPONENT=${COMPONENT}
+ENV=${ENV}
+TAILOR_ENV
+    # .. indenting heredocs is fragile as it requires tabs.
+    # matching tailor file
+    echo "Writing tailor file:  Tailorfile.${PROJECT}-${ENV}"
+    cat << TAILORFILE > "Tailorfile.${PROJECT}-${ENV}"
+namespace ${PROJECT}-${ENV}
+param-file ${PROJECT}-${ENV}.env
+svc,dc,is,bc
+ignore-path bc:/spec/output/to/name,bc:/spec/output/imageLabels
+TAILORFILE
+done
+
+# * does not expand in double quotes
+cp -v "${QS_DIR}"/ocp-templates/ocp-config/component-environment/*.yml .

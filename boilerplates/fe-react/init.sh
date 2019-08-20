a1b85dd0ac65e500ad1af79ca68a926b56625498
Maria Apsolon <maria.apsolon@boehringer-ingelheim.com>
2019-08-20 14:42:44 +0200

    separate npm commands for react jest-junit

--- boilerplates/fe-react/init.sh
+++ boilerplates/fe-react/init.sh
@@ -27,8 +27,9 @@ cd $TARGET_DIR
 
 echo "Init react app"
 sudo docker run --rm -v $PWD:/data -w /data node \
-  npm init react-app $COMPONENT --use-npm \
-  npm i jest-junit
+  npm init react-app $COMPONENT --use-npm 
+sudo docker run --rm -v $PWD:/data -w /data node \
+  cd $COMPONENT && npm install jest-junit
 
 cd $COMPONENT
 
